5682d5cf98aa3c79a73ae69fbcaae740324ec894
Angel Adrian Pedroza <dmydweb@hotmail.com>
2020-03-18 07:28:19 -0600

    Forms Main and Credentials
    Se agregan los formularios de Main y EditCredentials, que funcionara tanto para ediciones como para altas de credenciales

---
+++ Code/CredentialsManager.WinForms/Forms/EditCredential.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace CredentialsManager.WinForms.Forms
+{
+    partial class EditCredential
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtId = new System.Windows.Forms.TextBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtUrl = new System.Windows.Forms.TextBox();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.TxtNotes = new System.Windows.Forms.TextBox();
+            this.BtnNew = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnSaveAndClose = new System.Windows.Forms.Button();
+            this.BtnSaveAndNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Url:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Username:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Notes:";
+            // 
+            // TxtId
+            // 
+            this.TxtId.Location = new System.Drawing.Point(82, 22);
+            this.TxtId.Name = "TxtId";
+            this.TxtId.ReadOnly = true;
+            this.TxtId.Size = new System.Drawing.Size(100, 20);
+            this.TxtId.TabIndex = 6;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(82, 52);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(227, 20);
+            this.TxtName.TabIndex = 7;
+            // 
+            // TxtUrl
+            // 
+            this.TxtUrl.Location = new System.Drawing.Point(82, 84);
+            this.TxtUrl.Name = "TxtUrl";
+            this.TxtUrl.Size = new System.Drawing.Size(227, 20);
+            this.TxtUrl.TabIndex = 8;
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(82, 119);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(227, 20);
+            this.TxtUsername.TabIndex = 9;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(82, 154);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(227, 20);
+            this.TxtPassword.TabIndex = 10;
+            // 
+            // TxtNotes
+            // 
+            this.TxtNotes.Location = new System.Drawing.Point(82, 189);
+            this.TxtNotes.Multiline = true;
+            this.TxtNotes.Name = "TxtNotes";
+            this.TxtNotes.Size = new System.Drawing.Size(227, 108);
+            this.TxtNotes.TabIndex = 11;
+            // 
+            // BtnNew
+            // 
+            this.BtnNew.Location = new System.Drawing.Point(376, 22);
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.Size = new System.Drawing.Size(185, 23);
+            this.BtnNew.TabIndex = 12;
+            this.BtnNew.Text = "New";
+            this.BtnNew.UseVisualStyleBackColor = true;
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.Location = new System.Drawing.Point(376, 52);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(185, 23);
+            this.BtnEdit.TabIndex = 13;
+            this.BtnEdit.Text = "Edit";
+            this.BtnEdit.UseVisualStyleBackColor = true;
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(376, 81);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(185, 23);
+            this.BtnDelete.TabIndex = 14;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(376, 110);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(185, 23);
+            this.BtnSave.TabIndex = 15;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            // 
+            // BtnSaveAndClose
+            // 
+            this.BtnSaveAndClose.Location = new System.Drawing.Point(376, 141);
+            this.BtnSaveAndClose.Name = "BtnSaveAndClose";
+            this.BtnSaveAndClose.Size = new System.Drawing.Size(185, 23);
+            this.BtnSaveAndClose.TabIndex = 16;
+            this.BtnSaveAndClose.Text = "Save and Close";
+            this.BtnSaveAndClose.UseVisualStyleBackColor = true;
+            // 
+            // BtnSaveAndNew
+            // 
+            this.BtnSaveAndNew.Location = new System.Drawing.Point(376, 173);
+            this.BtnSaveAndNew.Name = "BtnSaveAndNew";
+            this.BtnSaveAndNew.Size = new System.Drawing.Size(185, 23);
+            this.BtnSaveAndNew.TabIndex = 17;
+            this.BtnSaveAndNew.Text = "Save and New";
+            this.BtnSaveAndNew.UseVisualStyleBackColor = true;
+            // 
+            // EditCredential
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 320);
+            this.Controls.Add(this.BtnSaveAndNew);
+            this.Controls.Add(this.BtnSaveAndClose);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnEdit);
+            this.Controls.Add(this.BtnNew);
+            this.Controls.Add(this.TxtNotes);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUsername);
+            this.Controls.Add(this.TxtUrl);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EditCredential";
+            this.Text = "Credential";
+            this.Load += new System.EventHandler(this.EditCredential_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtId;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtUrl;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.TextBox TxtNotes;
+        private System.Windows.Forms.Button BtnNew;
+        private System.Windows.Forms.Button BtnEdit;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnSaveAndClose;
+        private System.Windows.Forms.Button BtnSaveAndNew;
+    }
+}
---
+++ Code/CredentialsManager.WinForms/Forms/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CredentialsManager.WinForms.Forms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (EditCredential editCredential = new EditCredential()) 
+            {
+                editCredential.ShowDialog();
+            }
+        }
+    }
+}
