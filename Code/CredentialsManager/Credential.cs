663601c0a9672b1cee9c1a6bfa1078012719aecd
Angel Adrian Pedroza <dmydweb@hotmail.com>
2020-03-17 22:22:45 -0600

    Creando la clase Credential
    Se crea la clase Credential en su etapa inicial

---
+++ Code/CredentialsManager/Credential.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CredentialsManager
+{
+    public class Credential
+    {
+        public Credential() 
+        {
+        }
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Notes { get; set; }
+    }
+}
